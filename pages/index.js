623ab0087df328a1adb6a613603f90d87038214e
callmeeve <wididiky38@gmail.com>
2024-08-15 18:08:19 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
+        const res = await fetch("/api/projects");
         if (!res.ok) {
           throw new Error(`Failed to fetch projects: ${res.statusText}`);
         }
