196c8cfba481921337a4737a29e0269e055b4771
callmeeve <wididiky38@gmail.com>
2024-08-20 19:02:08 +0700

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useState, useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import { InfinitySpin } from "react-loader-spinner";
@@ -13,14 +14,36 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    <SessionProvider session={pageProps.session}>
-      {loading ? (
-        <div className="flex items-center justify-center min-h-screen">
-          <InfinitySpin visible={true} width="200" color="#06b6d4" />
-        </div>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </SessionProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Showcase projects website for Banyuwangi Developer"
+        />
+        <meta property="og:title" content="ShowcaseHub" />
+        <meta
+          property="og:description"
+          content="Showcase projects website for Banyuwangi Developer"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://showcase-hub-rho.vercel.app/"
+        />
+        <meta property="og:site_name" content="ShowcaseHub" />
+      </Head>
+      <SessionProvider session={pageProps.session}>
+        {loading ? (
+          <div className="flex items-center justify-center min-h-screen">
+            <InfinitySpin visible={true} width="200" color="#06b6d4" />
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </SessionProvider>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Showcase projects website for Banyuwangi Developer" />
-        <meta property="og:title" content="ShowcaseHub" />
-        <meta property="og:description" content="Showcase projects website for Banyuwangi Developer" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://showcase-hub-rho.vercel.app/" />
-        <meta property="og:site_name" content="ShowcaseHub" />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
